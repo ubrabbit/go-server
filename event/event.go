a70d136e9297b6407038f2f07e24cd95ff76f5cc
ubrabbit <ubrabbit@gmail.com>
2018-06-08 02:28:32 +0800

    利用管道解耦

--- event/event.go
+++ event/event.go
@@ -10,20 +10,26 @@ import (
 )
 
 var (
-	g_TriggerLen             = 1000
+	g_EventAdd               = 1
+	g_EventRemove            = 2
+	g_EventCmd               = 3
+	g_SignalLen              = 1000
 	g_GlobalEvent *EventPool = nil
 )
 
 type EventPool struct {
 	sync.Mutex
+	signalQueue chan *EventSignal
 	listenQueue map[string][]int
 	eventPool   map[int]interface{}
 }
 
 func NewEventPool() *EventPool {
 	obj := new(EventPool)
+	obj.signalQueue = make(chan *EventSignal, g_SignalLen)
 	obj.listenQueue = make(map[string][]int, 0)
 	obj.eventPool = make(map[int]interface{}, 0)
+	go obj.eventListener()
 	return obj
 }
 
@@ -31,6 +37,49 @@ type EventFunc interface {
 	Name() string
 	ID() int
 	Execute(...interface{})
+}
+
+type EventSignal struct {
+	Type int
+	Name string
+	Args []interface{}
+}
+
+func (self *EventSignal) String() string {
+	return fmt.Sprintf("[Signal][%s]-%d", self.Name, self.Type)
+}
+
+func (self *EventPool) pushEvent(name string, t int, args ...interface{}) bool {
+	self.signalQueue <- &EventSignal{Name: name, Type: t, Args: args}
+	return true
+}
+
+func (self *EventPool) eventListener() {
+	for {
+		obj := <-self.signalQueue
+		if obj == nil {
+			break
+		}
+		defer func() {
+			err := recover()
+			if err != nil {
+				LogError("Listen Signal Error: ", obj, err)
+			}
+		}()
+
+		switch obj.Type {
+		case g_EventAdd:
+			event := obj.Args[0]
+			self.addEvent(event)
+		case g_EventRemove:
+			id := obj.Args[0].(int)
+			self.removeEvent(id)
+		case g_EventCmd:
+			self.triggerEvent(obj.Name, obj.Args...)
+		default:
+			LogError("Invalid Event Type: ", obj.Type)
+		}
+	}
 }
 
 func (self *EventPool) addEvent(obj interface{}) bool {
@@ -99,15 +148,15 @@ func getEventPool() *EventPool {
 }
 
 func AddEvent(obj interface{}) bool {
-	return getEventPool().addEvent(obj)
+	return getEventPool().pushEvent("AddEvent", g_EventAdd, obj)
 }
 
 func RemoveEvent(id int) bool {
-	return getEventPool().removeEvent(id)
+	return getEventPool().pushEvent("RemoveEvent", g_EventRemove, id)
 }
 
 func TriggerEvent(name string, args ...interface{}) {
-	getEventPool().triggerEvent(name, args...)
+	getEventPool().pushEvent(name, g_EventCmd, args...)
 }
 
 func InitEvent() {
